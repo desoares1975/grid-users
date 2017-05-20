5ccfb63a702938fc96b11213194a77a173e87838
desoares <fabio.desoares@gmail.com>
2017-05-20 13:04:16 -0300

    pause for beer

--- app/server/index.js
+++ app/server/index.js
@@ -5,17 +5,54 @@ require('dotenv').config();
 const   express = require('express'),
         app = express(),
         http = require('http'),
-        Bluebird = require('bluebird');
+        Bluebird = require('bluebird'),
+        fs = Bluebird.promisifyAll(require('fs'));
 
 const server = http.createServer(app);
+
+app.set('view engine', 'pug');
+app.set('views', `${__base}app/server/views` );
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+console.log(__dirname + '/public');
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/vendors/css/bootstrap.css', (req, res) => {
+    let path = `${__base}node_modules/bootstrap/dist/css/bootstrap.min.css`;
+
+    fs.readFileAsync(path)
+    .then(data => {
+        res.status(200).send(data);
+    })
+    .catch(e => {
+        res.status(500).send(e);
+    });
+});
+
+// app.get('/vendors/:folder/:file', (req, res) => {
+//     let path = `${__base}node_modules/`;
+//     if (req.params.file === 'bootstrap.css') {
+//         path += ''
+//     }
+
+//     if(req.params.)
+
+//     if (req.params.folder === 'js') {
+
+//     }
+
+//     fs.readFileAsync(path)
+//     .then(data => {
+//         res.status(200).send(data);
+//     })
+//     .catch(e => {
+//         res.status(500).send(e);
+//     });
+// });
 
 server.listen(process.env.PORT, () => {
     console.log('Grid User Up and Runnign!!!! on port ', process.env.PORT);
 });
-
-app.get('/', (req, res) => {
-    return res.status(200).send('FU');
-});
-
-
-app.use('/public', express.static('public'));
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+global.__base = __dirname + '/';
+require('dotenv').config();
+require(`${__base}app`);
